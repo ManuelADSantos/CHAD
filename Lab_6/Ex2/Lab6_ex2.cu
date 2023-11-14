1f2694c53639af74e3aee00347b81a7b4ca2cb6c
ManuelADSantos <manel.a.d.santos@gmail.com>
2023-11-14 18:50:43 +0000

    Mostrar numero de pontos

--- Lab_6/Ex2/Lab6_ex2.cu
+++ Lab_6/Ex2/Lab6_ex2.cu
@@ -11,7 +11,7 @@
 #include <curand_kernel.h>
 #include <cuda_runtime.h>
 
-__global__ void get_pi(int *count_circle, int *count_square, int seed)
+__global__ void get_pi(unsigned long long int *count_circle, unsigned long long int *count_square, int seed)
 {
     int idx = threadIdx.x + blockIdx.x * blockDim.x;
 
@@ -33,25 +33,29 @@ __global__ void get_pi(int *count_circle, int *count_square, int seed)
 
 int main(int argc, char *argv[])
 {   
-    int circle_host = 0, square_host = 0;
-    int *circle_device, *square_device;
+    unsigned long long int circle_host = 0, square_host = 0;
+    unsigned long long int *circle_device, *square_device;
     int time_host = time(NULL);
 
-    cudaMalloc((void**)&circle_device, sizeof(int));
-    cudaMalloc((void**)&square_device, sizeof(int));
+    cudaMalloc((void**)&circle_device, sizeof(unsigned long long int));
+    cudaMalloc((void**)&square_device, sizeof(unsigned long long int));
 
-    cudaMemcpy(circle_device, &circle_host, sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(square_device, &square_host, sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(circle_device, &circle_host, sizeof(unsigned long long int), cudaMemcpyHostToDevice);
+    cudaMemcpy(square_device, &square_host, sizeof(unsigned long long int), cudaMemcpyHostToDevice);
 
     int threadsPerBlock = 256;
     int blocksPerGrid = 256;
 
     get_pi<<<blocksPerGrid, threadsPerBlock>>>(circle_device, square_device, time_host);
 
-    cudaMemcpy(&circle_host, circle_device, sizeof(int), cudaMemcpyDeviceToHost);
-    cudaMemcpy(&square_host, square_device, sizeof(int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(&circle_host, circle_device, sizeof(unsigned long long int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(&square_host, square_device, sizeof(unsigned long long int), cudaMemcpyDeviceToHost);
 
-    printf("Pi = %f\n", 4.0*circle_host/(circle_host+square_host));
+    printf("Pi = %.10g\n", (double)(4.0*circle_host/(double)(circle_host+square_host)));
+    printf("Points: Circle = %llu || Square = %llu\n", circle_host, square_host);
+
+    cudaFree(circle_device);
+    cudaFree(square_device);
 
     return 0;
 }
