4a2642116b4d559adcd67d503e84c71c6f3ff144
Manuel Santos <manel.a.d.santos@gmail.com>
2023-11-19 21:04:58 +0000

    Ex base

---
+++ Lab_6/Ex3/Lab6_ex2.cu
@@ -0,0 +1,88 @@
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image.h"
+#include "stb_image_write.h"
+#include <iostream>
+#define BLUR_SIZE 16
+#define R 0
+#define G 1
+#define B 2
+#define TILE_DIM 16
+#define BLOCK_SIZE 16
+#define BLOCK_W (TILE_DIM+(2*raio))
+#define BLOCK_H (TILE_DIM+(2*raio))
+#define raio 2
+
+__global__ void blurKernel(unsigned char* in, unsigned char* out, int width, int height, int num_channel, int channel) {
+
+  //a)
+  int col = blockIdx.x * TILE_DIM + threadIdx.x - raio;
+  int row = blockIdx.y * TILE_DIM + threadIdx.y - raio;
+  
+  //b)
+  //__shared__ unsigned char tile[BLOCK_W*BLOCK_H];
+  
+  //int col = blockIdx.x * TILE_DIM + threadIdx.x - raio;
+  //int row = blockIdx.y * TILE_DIM + threadIdx.y - raio;
+    
+  int pixVal;
+  int pixels;
+  if(col > -1 && col < width && row > -1 && row < height ) {
+    pixVal = 0;
+    pixels = 0;
+    //tile[row*TILE_DIM*num_channel + col*num_channel] = in[row*width*num_channel + col*num_channel];
+    //__syncthreads();
+    for(int blurRow = -BLUR_SIZE; blurRow < BLUR_SIZE + 1; ++blurRow) {
+      for(int blurCol = -BLUR_SIZE; blurCol < BLUR_SIZE + 1; ++blurCol) {
+        int curRow = row + blurRow;
+        int curCol = col + blurCol;
+        if(curRow > -1 && curRow < height && curCol > -1 && curCol < width) {
+          pixVal += in[curRow * width * num_channel + curCol * num_channel + channel]; // a)
+          //pixVal += tile[curRow * width * num_channel + curCol * num_channel + channel]; // b)
+          pixels++;
+          //__syncthreads();
+        }
+      }
+    }
+    out[row * width * num_channel + col * num_channel + channel] = (unsigned char)(pixVal/pixels);
+  }
+}
+
+int main() {
+
+  int width, height,n;
+  unsigned char *image = stbi_load("800x600Tanjiro.jpg",&width,&height,&n,0);
+  unsigned char *output = (unsigned char*)malloc(width * height * n *sizeof(unsigned char));
+  unsigned char* Dev_Input_Image = NULL;
+  unsigned char* Dev_Output_Image = NULL;
+  cudaMalloc((void**)&Dev_Input_Image, sizeof(unsigned char)* height * width * n);
+  cudaMalloc((void**)&Dev_Output_Image, sizeof(unsigned char)* height * width * n);
+
+  //CLOCK_PROCESS_CPUTIME_ID - Profiling the execution time
+  struct timespec start, end;
+  clock_gettime(CLOCK_MONOTONIC, &start);
+  
+  cudaMemcpy(Dev_Input_Image, image, sizeof(unsigned char) * height * width * n, cudaMemcpyHostToDevice);
+
+  //kernel call
+  dim3 blockSize(BLOCK_SIZE, BLOCK_SIZE);
+  dim3 gridSize(width/blockSize.x+1, height/blockSize.y+1);
+  blurKernel <<<gridSize, blockSize>>>(Dev_Input_Image, Dev_Output_Image, width, height,n,R);
+  blurKernel <<<gridSize, blockSize>>>(Dev_Input_Image, Dev_Output_Image, width, height,n,G);
+  blurKernel <<<gridSize, blockSize>>>(Dev_Input_Image, Dev_Output_Image, width, height,n,B);
+  
+  cudaDeviceSynchronize(); // we need this so the kernel is guaranteed to finish (and the output from the kernel will find a waiting standard output queue), before the application is allowed to exit
+
+  cudaMemcpy(image, Dev_Output_Image, sizeof(unsigned char) * height * width * n, cudaMemcpyDeviceToHost);
+  clock_gettime(CLOCK_MONOTONIC, &end);
+    
+  cudaFree(Dev_Input_Image);
+  cudaFree(Dev_Output_Image);
+  stbi_write_jpg("output_stbimage.jpg", width, height, n, image, width * n);
+  
+  double initialTime=(start.tv_sec*1e3)+(start.tv_nsec*1e-6);
+  double finalTime=(end.tv_sec*1e3)+(end.tv_nsec*1e-6);
+  printf("Ex 2: %f ms\n", (finalTime - initialTime));
+
+  return 0;
+}
